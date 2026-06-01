9506bee0dc60a9a3f567a0c38ded2cbb73ba88de
Nikolay Nedelchev <ndn@Nikolays-MacBook-Pro.local>
2026-06-01 15:11:33 +0300

    cleanup

--- cores/altirrasdl/src/Altirra/h/uiprofiler.h
+++
@@ -1,9 +0,0 @@
-#ifndef f_AT_UIPROFILER_H
-#define f_AT_UIPROFILER_H
-
-class ATUIManager;
-
-void ATUIProfileCreateWindow(ATUIManager *m);
-void ATUIProfileDestroyWindow();
-
-#endif
--- cores/altirrasdl/src/h/vd2/Tessa/Config.h
+++
@@ -1,7 +0,0 @@
-#ifndef f_VD2_TESSA_CONFIG_H
-#define f_VD2_TESSA_CONFIG_H
-
-void VDTSetLibraryOverridesEnabled(bool enabled);
-bool VDTGetLibraryOverridesEnabled();
-
-#endif	// f_VD2_TESSA_CONFIG_H
